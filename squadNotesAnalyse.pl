701ba513549a8fb4ac67f3176919c5ef6f93f939
Stewart Fraser <stewart.fraser@f-e-t.com>
2016-12-23 14:10:14 +0000

    Writing to new file nicely formatted.

--- squadNotesAnalyse.pl
+++ squadNotesAnalyse.pl
@@ -2,6 +2,12 @@
 
 # runs with Strawberry Perl: http://strawberryperl.com/
 
+# Copy and paste the SquadNotes sheet "date, GBP and comments" to a new spreadsheet
+# Save this spreadsheet as a CSV file with the following options
+# file -> save as -> csv (edit filter settings, text csv format, ... 
+# ... character set Western Europe, field delimiter :, Text delimiter ", ...
+# ... save cell contents as shown, quote all text cells)
+
 use strict;
 use warnings;
 use feature qw(say);
@@ -22,13 +28,12 @@ my $folderToAnalyse;
 my $fullPathname;
 my @splitter;
 my $poundSign = chr(156);
-my $newfile;
+my $formattedFile;
 
 $folderToAnalyse = "F:\\dev\\abosSpreadsheet";
 checkFolderExists($folderToAnalyse) ? 1 : exit;
 
-# find all the txt files from the folder (and its sub-folders) to analyse
-say "\n\n\n******* sheet.txt *******\n\n";
+# find "sheet.txt" from the folder (and its sub-folders) to analyse
 find( \&fileWanted, $folderToAnalyse); 
 analyseFiles(\@contentFile);
 
@@ -60,87 +65,191 @@ sub analyseFiles {
     foreach my $file (@arrOfProjFilePaths) {
         my @afile = read_file($file);
         my @payments;
+        my @paymentsCash;
+        my @paymentsDate;
         my @miscs;
+        my @miscsCash;
+        my @miscsDate;
         my @referees;
+        my @refereesCash;
+        my @refereesDate;
         my @pitches;
+        my @pitchesCash;
+        my @pitchesDate;
         my @refunds;
+        my @refundsCash;
+        my @refundsDate;
+        my @fundraisings;
+        my @fundraisingsCash;
+        my @fundraisingsDate;
+        my @facilities;
+        my @facilitiesCash;
+        my @facilitiesDate;
         foreach my $line (@afile){
             # ignore lines that are just whitespace
             ($line =~ /^\s*$/) ? next : 1;
 
             # payments 
-            if ($line =~ / payment/) {
-                # split at "£" (use utf8)
-                my @quids = split /£/, $line;
-                # split at spaces " "
-                my @spaces = split / /, $quids[1];
-                push @payments, $spaces[0];
+            if ($line =~ /\"Abo payment/) {
+                # split at ":"
+                my @cols = split /:/, $line;
+                push @payments, $cols[2];
+                push @paymentsCash, $cols[1];
+                push @paymentsDate, $cols[0];
             }
-
-#
-#
-#
-#
-# TODO: split Misc. into paid and received on spreadsheet
-#
-#
-#
-            # miscelleneous
-            if ($line =~ /Misc\./) {
-                # split at "£" (use utf8)
-                my @quids = split /£/, $line;
-                my @spaces = split / /, $quids[1];
-                push @miscs, $spaces[0];
+            
+            # miscelleneous spend
+            if ($line =~ /\"Miscellaneous spend/) {
+                # split at ":"
+                my @cols = split /:/, $line;
+                push @miscs, $cols[2];
+                push @miscsCash, $cols[1];
+                push @miscsDate, $cols[0];
             }
             
             # referees
-            if ($line =~ / referee/) {
-                # split at "£" (use utf8)
-                my @quids = split /£/, $line;
-                my @spaces = split / /, $quids[1];
-                push @referees, $spaces[0];
+            if ($line =~ /\"Referee /) {
+                # split at ":"
+                my @cols = split /:/, $line;
+                push @referees, $cols[2];
+                push @refereesCash, $cols[1];
+                push @refereesDate, $cols[0];
             }
-
+            
             # pitches
-            if ($line =~ / pitch/) {
-                # split at "£" (use utf8)
-                my @quids = split /£/, $line;
-                my @spaces = split / /, $quids[1];
-                push @pitches, $spaces[0];
+            if ($line =~ /\"Pitch /) {
+                # split at ":"
+                my @cols = split /:/, $line;
+                push @pitches, $cols[2];
+                push @pitchesCash, $cols[1];
+                push @pitchesDate, $cols[0];
             }
             
             # refunds
-            if ($line =~ / refund/) {
-                # split at "£" (use utf8)
-                my @quids = split /£/, $line;
-                my @spaces = split / /, $quids[1];
-                push @refunds, $spaces[0];
+            if ($line =~ /\"Refund /) {
+                # split at ":"
+                my @cols = split /:/, $line;
+                push @refunds, $cols[2];
+                push @refundsCash, $cols[1];
+                push @refundsDate, $cols[0];
+            }
+            
+            # fundraising
+            if ($line =~ /\"Fundraising /) {
+                # split at ":"
+                my @cols = split /:/, $line;
+                push @fundraisings, $cols[2];
+                push @fundraisingsCash, $cols[1];
+                push @fundraisingsDate, $cols[0];
+            }
+            
+            # facilities
+            if ($line =~ /\"Facility /) {
+                # split at ":"
+                my @cols = split /:/, $line;
+                push @facilities, $cols[2];
+                push @facilitiesCash, $cols[1];
+                push @facilitiesDate, $cols[0];
             }
         }
-        #foreach my $payment (@payments){
-        #    say $payment;
-        #}
-        #foreach my $misc (@miscs){
-        #    say $misc;
-        #}
-        #foreach my $referee (@referees){
-        #    say $referee;
-        #}
-        #foreach my $pitch (@pitches){
-        #    say $pitch;
-        #}
-        foreach my $refund (@refunds){
-            say $refund;
-        }
+
+        my $paymentsSum = 0; 
+        my $miscsSum = 0; 
+        my $refereesSum = 0; 
+        my $pitchesSum = 0; 
+        my $refundsSum = 0; 
+        my $fundraisingsSum = 0; 
+        my $facilitiesSum = 0; 
+
         # remove the filename extension and rename
-        ($newfile = $file) =~ s/\.[^.]+$//;
-        $newfile = $newfile."New.txt";
-        # write the concatenated array to file
-        #open(my $fh, '>', $newfile) or die "Could not open file '$newfile' $!";
-        #foreach my $line (@dates){
-        #    print $fh $line;
-        #}
-        #close $fh;
+        ($formattedFile = $file) =~ s/\.[^.]+$//;
+        $formattedFile= $formattedFile."Formatted.txt";
+        # write formatted information to file
+        open(my $fh, '>', $formattedFile) or die "Could not open file '$formattedFile' $!";
+
+
+        printf $fh "\n";
+        printf $fh "********** Payments from Abos **********\n";
+        for(my $i=0; $i<scalar(@payments); $i++){
+            $paymentsSum += $paymentsCash[$i];
+            chomp $payments[$i];
+            printf $fh ("%-10d %-10.2f %s\n", $paymentsDate[$i], $paymentsCash[$i], $payments[$i]);
+        }
+        printf $fh "Total payments from Abos = £$paymentsSum\n";
+
+        printf $fh "\n";
+        printf $fh "\n";
+        printf $fh "********** Miscellaneous Spends **********\n";
+        for(my $i=0; $i<scalar(@miscs); $i++){
+            $miscsSum += $miscsCash[$i];
+            chomp $miscs[$i];
+            printf $fh ("%-10d %-10.2f %s\n", $miscsDate[$i], $miscsCash[$i], $miscs[$i]);
+        }
+        printf $fh "Total miscelleneous spends= £$miscsSum\n";
+
+        printf $fh "\n";
+        printf $fh "\n";
+        printf $fh "********** Referee costs **********\n";
+        for(my $i=0; $i<scalar(@referees); $i++){
+            $refereesSum += $refereesCash[$i];
+            chomp $referees[$i];
+            printf $fh ("%-10d %-10.2f %s\n", $refereesDate[$i], $refereesCash[$i], $referees[$i]);
+        }
+        printf $fh "Total referee costs = £$refereesSum\n";
+
+        printf $fh "\n";
+        printf $fh "\n";
+        printf $fh "********** Pitch costs **********\n";
+        for(my $i=0; $i<scalar(@pitches); $i++){
+            $pitchesSum += $pitchesCash[$i];
+            chomp $pitches[$i];
+            printf $fh ("%-10d %-10.2f %s\n", $pitchesDate[$i], $pitchesCash[$i], $pitches[$i]);
+        }
+        printf $fh "Total pitch costs = £$pitchesSum\n";
+
+        printf $fh "\n";
+        printf $fh "\n";
+        printf $fh "********** Refunds **********\n";
+        for(my $i=0; $i<scalar(@refunds); $i++){
+            $refundsSum += $refundsCash[$i];
+            chomp $refunds[$i];
+            printf $fh ("%-10d %-10.2f %s\n", $refundsDate[$i], $refundsCash[$i], $refunds[$i]);
+        }
+        printf $fh "Total refunds = £$refundsSum\n";
+
+        printf $fh "\n";
+        printf $fh "\n";
+        printf $fh "********** Fundraising **********\n";
+        for(my $i=0; $i<scalar(@fundraisings); $i++){
+            $fundraisingsSum += $fundraisingsCash[$i];
+            chomp $fundraisings[$i];
+            printf $fh ("%-10d %-10.2f %s\n", $fundraisingsDate[$i], $fundraisingsCash[$i], $fundraisings[$i]);
+        }
+        printf $fh "Total fundraisings = £$fundraisingsSum\n";
+
+        printf $fh "\n";
+        printf $fh "\n";
+        printf $fh "********** Facilities **********\n";
+        for(my $i=0; $i<scalar(@facilities); $i++){
+            $facilitiesSum += $facilitiesCash[$i];
+            chomp $facilities[$i];
+            printf $fh ("%-10d %-10.2f %s\n", $facilitiesDate[$i], $facilitiesCash[$i], $facilities[$i]);
+        }
+        printf $fh "Total cost for facilities = £$facilitiesSum\n";
+
+        printf $fh "\n";
+        printf $fh "\n";
+        printf $fh "********** Summary **********\n";
+        printf $fh "Total payments from Abos = £$paymentsSum\n";
+        printf $fh "Total miscelleneous spends= £$miscsSum\n";
+        printf $fh "Total referee costs = £$refereesSum\n";
+        printf $fh "Total pitch costs = £$pitchesSum\n";
+        printf $fh "Total refunds = £$refundsSum\n";
+        printf $fh "Total fundraisings = £$fundraisingsSum\n";
+        printf $fh "Total cost for facilities = £$facilitiesSum\n";
+
+        # close the file
+        close $fh;
     }
 }
 
@@ -172,4 +281,3 @@ sub checkFolderExists {
         return 0;
     }
 }
-
